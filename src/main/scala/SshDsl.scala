cf8474f4b7570e74ea1b1e2a466a17d14134cd0f
Crosson David <crosson.david@gmail.com>
2017-10-03 15:14:02 +0200

    dsl is ready

--- src/main/scala/SshDsl.scala
+++ src/main/scala/SshDsl.scala
@@ -4,17 +4,11 @@ object SshDsl {
   import fr.janalyse.ssh._
   import com.jcraft.jsch.ProxyHTTP
 
-  implicit class ProxyEndPointHelper(val sc:StringContext) extends AnyVal {
+
+  implicit class ProxyEndPointStringInterpolator(val sc:StringContext) extends AnyVal {
     def proxy(args: Any*):ProxyEndPoint = {
-      val strings = sc.parts.iterator
-      val expressions = args.iterator
-      var buf = new StringBuffer(strings.next)
-      while(strings.hasNext) {
-        buf append expressions.next
-        buf append strings.next
-      }
       val EpRE="""([^~]+)(?:~(\d+))?""".r
-      buf.toString match {
+      sc.s(args: _*) match {
         case EpRE(host,port) =>  // TODO : not safe
           ProxyEndPoint(
             host=host,
@@ -24,17 +18,10 @@ object SshDsl {
     }
   }
 
-  implicit class SshEndPointHelper(val sc:StringContext) extends AnyVal {
+  implicit class SshEndPointStringInterpolator(val sc:StringContext) extends AnyVal {
     def ssh(args: Any*):SshEndPoint = {
-      val strings = sc.parts.iterator
-      val expressions = args.iterator
-      var buf = new StringBuffer(strings.next)
-      while(strings.hasNext) {
-        buf append expressions.next
-        buf append strings.next
-      }
       val EpRE="""(?:([a-zA-Z0-9_]+)@)?([^~]+)(?:~(\d+))?""".r
-      buf.toString match {
+      sc.s(args: _*) match {
         case EpRE(user,host,port) =>  // TODO : not safe
           SshEndPoint(
             host=host,
@@ -45,66 +32,41 @@ object SshDsl {
     }
   }
 
-
-
-  implicit class EndPointToAccessPath(from:EndPoint) {
-    def <~>(to:EndPoint):AccessPath = AccessPath(from::to::Nil)
+  implicit def tupleToAccessPath(tuple:Tuple2[String, EndPoint]): AccessPath = {
+    tuple match {case (name,from) => AccessPath(name,from::Nil)}
   }
-
-  implicit class AccessPathHelper(path:AccessPath) {
-    def <~>(to:EndPoint):AccessPath = path.copy(path.endpoints:+to)
-  }
-
-  class ServerContext(name:String) {
-    var path = AccessPath(SshEndPoint("127.0.0.1")::Nil)
-    def path_(newPath:AccessPath):Unit = {path=newPath}
-    def pshell[T] (proc : SSHShell => T):T = {
-      def intricate(endpoints:Iterable[EndPoint], lport:Option[Int]=None, through:Option[ProxyEndPoint]=None):T = {
-        endpoints.headOption match {
-          case Some(endpoint:ProxyEndPoint) =>
-            intricate(endpoints.tail, lport, Some(endpoint))
-          case Some(endpoint:SshEndPoint) if lport.isDefined => // intricate tunnel
-            val proxy = through.map(p => new ProxyHTTP(p.host, p.port))
-            val opts = SSHOptions("127.0.0.1", username=endpoint.username, port=lport.get, proxy=proxy)
-            SSH.once(opts) { ssh =>
-              val newPort = ssh.remote2Local(endpoint.host,endpoint.port)
-              intricate(endpoints.tail, Some(newPort))
-           }
-          case Some(endpoint:SshEndPoint)  => // first tunnel
-            val proxy = through.map(p => new ProxyHTTP(p.host, p.port))
-            val opts=SSHOptions(endpoint.host, username=endpoint.username, port=endpoint.port, proxy=proxy)
-            SSH.once(opts) { ssh =>
-              val newPort = ssh.remote2Local("127.0.0.1",22)
-              intricate(endpoints.tail, Some(newPort))
-           }
-          case None if lport.isDefined => SSH.shell("localhost",port=lport.get) { proc }
-          case None =>
-            throw new RuntimeException("Empty ssh path")
-        }
-      }
-      intricate(path.endpoints)
-    }
-    def shell[T] (withSh : implicit SSHShell => T):T = {
-      pshell { sh =>
-        implicit val ish=sh
-        withSh
-      }
+  implicit class EndPointToAccessPath(from:Tuple2[String, EndPoint]) {
+    def ^(to:EndPoint):AccessPath = from match {
+      case (name, from) => AccessPath(name, from::to::Nil)
     }
   }
-  var servers = Map.empty[String,ServerContext]
 
-  def server(name:String)(specify: implicit ServerContext => Unit) = {
-    implicit val context = new ServerContext(name)
-    servers+=name->context
-    specify
+  implicit class AccessPathHelper(accessPath:AccessPath) {
+    def ^(next:EndPoint):AccessPath= accessPath.copy(endpoints = accessPath.endpoints:+next)
   }
-  def access(path:AccessPath)(implicit context:ServerContext) = {
-    context.path=path
+
+
+  class SessionHelper() {
+    var accesses:List[AccessPath]=Nil
+    def addAccess(access:AccessPath):Unit = accesses = access::accesses
   }
-  def shell[T](serverName:String)(withSh : implicit SSHShell => T):T = {
-    servers(serverName).pshell { sh =>
-      implicit val ish = sh
-      withSh
+
+  def session(accesses: implicit SessionHelper => Unit)(operations: implicit SSHConnectionManager => Unit) = {
+    implicit val sessionHelper = new SessionHelper
+    accesses
+    implicit val connectionManager = SSHConnectionManager(sessionHelper.accesses)
+    operations
+    connectionManager.close()
+  }
+
+  def server(access:AccessPath)(implicit sessionHelper: SessionHelper):Unit = {
+    sessionHelper.addAccess(access)
+  }
+
+  def shell(name:String)(operations: implicit SSHShell => Unit)(implicit manager: SSHConnectionManager):Unit = {
+    manager.shell(name) { sh =>
+      implicit val shi = sh
+      operations
     }
   }
 
@@ -113,22 +75,25 @@ object SshDsl {
   def pwd(implicit sh:SSHShell) = {sh.pwd()}
   def mkdir(name:String)(implicit sh:SSHShell) = {sh.mkdir(name)}
   def exec(cmd:String)(implicit sh:SSHShell) = {sh.execute(cmd)}
+  def whoami(implicit sh:SSHShell) = {sh.whoami}
+  def hostname(implicit sh:SSHShell) = {sh.hostname}
+  def uptime(implicit sh:SSHShell) = {sh.uptime}
 }
 
 
 
 object test {
   def main(args: Array[String]): Unit = {
-    import SshDsl._
-    server("srv1") {
-      //access(proxy"127.0.0.1~3128" <~> ssh"dcr@127.0.0.1~22" <~> ssh"test@127.0.0.1~22")
-      access(ssh"dcr@127.0.0.1~22" <~> ssh"test@127.0.0.1~22")
-    }
 
-    shell("srv1") {
-       ls.take(5).map{println}
-       println("=> I AM "+exec("whoami"))
-    } 
+    import SshDsl._
+
+    session {
+      server("srv1" -> ssh"dcr@127.0.0.1~22")
+      server("srv2" -> proxy"127.0.0.1" ^ ssh"dcr@127.0.0.1" ^ ssh"test@127.0.0.1")
+    } {
+      shell("srv1") { println(s"Hello from $hostname $whoami $pwd") }
+      shell("srv2") { println(s"Hello from $hostname $whoami $pwd") }
+    }
     
   }
 }
